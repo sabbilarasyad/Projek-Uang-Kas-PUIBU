6e15ef7b1be56bae96a2e9e24f5b19dc9a97078b
sabil <rasyabilaa14@gmail.com>
2026-03-26 15:24:06 +0700

    Add footer section to footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+</head>
+    <!-- Footer -->
+    <footer class="footer">
+        <div class="container">
+            <p>&copy; <?php echo date("Y"); ?> PUIBU SMKN 1 KATAPANG</p>
+        </div>
+    </footer>
